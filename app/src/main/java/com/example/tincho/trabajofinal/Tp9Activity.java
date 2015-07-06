50317b42263f7723e204fc2e959fbe7eb14d1416
tinchop14 <tinchop.14@gmail.com>
2015-07-06 12:30:07 -0300

    Agregada activity tp9

---
+++ app/src/main/java/com/example/tincho/trabajofinal/Tp9Activity.java
@@ -0,0 +1,45 @@
+package com.example.tincho.trabajofinal;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Tp9Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tp9);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tp9, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_tp5) {
+
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+            this.finish();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
