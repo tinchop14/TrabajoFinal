5336ff4b00ff6f74bd941d0b900f164a99928d63
tinchop14 <tinchop.14@gmail.com>
2015-07-06 16:33:31 -0300

    Vista tp9

--- app/src/main/java/com/example/tincho/trabajofinal/Tp9Activity.java
+++ app/src/main/java/com/example/tincho/trabajofinal/Tp9Activity.java
@@ -5,14 +5,58 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 
 public class Tp9Activity extends AppCompatActivity {
+
+    private ImageView imageViewObject;
+    private ImageView imageViewOp2;
+    private ImageView imageViewOp3;
+    private ImageView imageViewOp4;
+    private ImageView imageViewOp5;
+    private ImageView imageViewOp1;
+    private String option;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tp9);
+
+        imageViewObject = (ImageView) findViewById(R.id.imageObject);
+
+        imageViewOp1 = (ImageView) findViewById(R.id.imageL1);
+        imageViewOp2 = (ImageView) findViewById(R.id.imageL2);
+        imageViewOp3 = (ImageView) findViewById(R.id.imageL3);
+        imageViewOp4 = (ImageView) findViewById(R.id.imageL4);
+        imageViewOp5 = (ImageView) findViewById(R.id.imageL5);
+
+        option = generateQuestion();
+
+
+
+    }
+
+    private String generateQuestion() {
+
+        String[] list = new String[]{"bicicleta", "botellas", "burro", "casa", "cepillo", "cuna"
+                , "dado", "dedo", "domino", "foca", "fuego", "gato", "gusano", "jirafa", "lapiz",
+                "leche", "loro", "luna", "mano", "mesa", "mono", "muneca", "nube", "pato", "pera",
+                "puerta", "raton", "reloj", "rueda", "silla", "tenedor", "tijera", "toro", "vaso",
+                "Queso", "yoyo", "zapato", "zorro"};
+
+        ArrayList<String> objects = new ArrayList<>();
+        objects.addAll(Arrays.asList(list));
+
+        Random r = new Random();
+
+        return objects.get(r.nextInt(37));
+
     }
 
     @Override
